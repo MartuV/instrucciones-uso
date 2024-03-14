95a95d61928d4790232a88f23392573ab6955fd5
MartuV <serranomv@hotmail.com>
2024-03-13 22:55:34 -0300

    mejoras en el footer y nadvar

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import footer from '../estilos/footer.css';
+import '../estilos/footer.css'; // Importa los estilos CSS
 
 function Footer() {
     const year = new Date().getFullYear(); // Obtiene el año actual
 
     return (
         <footer className="footer">
-            <div className="footer-left">
-                <p>@ {year}, </p>
+            <div className="footer-content">
+                <p>Información Medicamentos: Nombre</p>
+                <p>Desarrollo Web: Tu Nombre</p>  
             </div>
-            <div className="footer-right">
-            <p>Información proporcionada por: Tu Nombre</p>
-            <p>Desarrollo web por: Tu Nombre</p>  
-                
+            <div className="footer-year">
+                <p>@ {year}</p>
             </div>
         </footer>
     );
 }
 
 export default Footer;
+
