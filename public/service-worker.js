0e69ea5a9e3b07a2a40af7a929ef46e83c404606
MartuV <serranomv@hotmail.com>
2024-04-05 16:51:21 -0300

    trabajar sin conexion prueba 1

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,85 @@
-// Archivo service-worker.js
 const CACHE_NAME = 'mi-app-cache-v1';
-const offlinePage = '/offline.html';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+
+  '/componentes/Aerosoles.js',
+  '/componentes/Footer.js',
+  '/componentes/Jarabes.js',
+  '/componentes/MenuQR.js',
+  '/componentes/Navbar.js',
+  '/componentes/Tarjeta.js',
+ 
+
+  '/estilos/footer.css',
+  '/estilos/instrucciones.css',
+  '/estilos/menuQR.css',
+  '/estilos/navbar.css',
+
+
+
+  '/imagenes/aerocamara.png',
+  '/imagenes/aerosol.png',
+  '/imagenes/agitar.png',
+  '/imagenes/contento.png',
+];
 
 self.addEventListener('install', event => {
+  // Instalar el Service Worker
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.add(offlinePage))
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Variable para almacenar si el mensaje de instalación se ha mostrado
+let mensajeInstalacionMostrado = false;
+
 self.addEventListener('fetch', event => {
+  // Intercepta solicitudes de red
   event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(offlinePage))
+    caches.match(event.request)
+      .then(response => {
+        // Retorna el recurso desde la caché si está disponible
+        return response || fetch(event.request);
+      })
   );
 });
+
+self.addEventListener('beforeinstallprompt', (event) => {
+  // Previene que el navegador muestre automáticamente el mensaje de instalación
+  event.preventDefault();
+  // Almacena el indicador en el almacenamiento local del navegador
+  if (!mensajeInstalacionMostrado) {
+    mensajeInstalacionMostrado = true;
+    mostrarMensajeInstalacion();
+  }
+});
+
+function mostrarMensajeInstalacion() {
+  // Aquí puedes mostrar el mensaje de instalación, por ejemplo, en respuesta a una acción del usuario
+  // Por ejemplo, podrías mostrar un botón que diga "Instalar PWA" y al hacer clic en él, llamas a prompt() en el evento deferredPrompt
+  const botonInstalarPWA = document.getElementById('botonInstalarPWA');
+  // Verificar si la aplicación ya está instalada
+  if (window.matchMedia('(display-mode: standalone)').matches) {
+    // Si está instalada, ocultar el botón de instalación
+    botonInstalarPWA.style.display = 'none';
+  } else {
+    // Si no está instalada, mostrar el botón de instalación
+    botonInstalarPWA.addEventListener('click', () => {
+      // Muestra el mensaje de instalación
+      deferredPrompt.prompt();
+      // Espera a que el usuario responda a la solicitud de instalación
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('El usuario ha aceptado la instalación');
+        } else {
+          console.log('El usuario ha cancelado la instalación');
+        }
+        // Limpia el objeto deferredPrompt
+        deferredPrompt = null;
+      });
+    });
+  }
+} 
--- src/componentes/MenuQR.js
+++ src/componentes/MenuQR.js
@@ -5,6 +5,7 @@ import OfflineMessage from './OfflineMessage';
 import Tarjeta from './Tarjeta';
 
 function MenuQR() {
+  
   const qrImages = [
     { nombre: 'Instrucciones 1', imagen: 'jarabe.png', grupo: 'Antibióticos Jarabe - Polvo para Reconstituir Vía Oral', medicamentos: 'Amoxicilina - Amoxicilina/Clavulánico - Cefalexina - Claritromicina' },
     { nombre: 'Instrucciones 2', imagen: 'inhalador.png', grupo: 'Aerosoles y Aerocámaras', medicamentos: 'Budesonide - Salbutamol' },
@@ -12,18 +13,21 @@ function MenuQR() {
 
   const [isInstalled, setIsInstalled] = useState(false);
   const [deferredPrompt, setDeferredPrompt] = useState(null);
-  const [isOnline, setIsOnline] = useState(navigator.onLine);
 
   useEffect(() => {
     const beforeInstallPromptHandler = (event) => {
+      // Prevenir que el navegador muestre automáticamente el mensaje de instalación
       event.preventDefault();
+      // Guardar el evento para usarlo más tarde
       setDeferredPrompt(event);
     };
 
     const appInstalledHandler = () => {
+      // La aplicación se ha instalado, actualiza el estado para ocultar el botón de instalación
       setIsInstalled(true);
     };
 
+    // Verificar si la PWA ya está instalada en el dispositivo
     if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true) {
       setIsInstalled(true);
     }
@@ -37,60 +41,46 @@ function MenuQR() {
     };
   }, []);
 
-  useEffect(() => {
-    const handleOnlineStatus = () => {
-      setIsOnline(navigator.onLine);
-    };
-
-    window.addEventListener('online', handleOnlineStatus);
-    window.addEventListener('offline', handleOnlineStatus);
-
-    return () => {
-      window.removeEventListener('online', handleOnlineStatus);
-      window.removeEventListener('offline', handleOnlineStatus);
-    };
-  }, []);
-
   const mostrarMensajeInstalacion = () => {
     if (deferredPrompt) {
+      // Mostrar el mensaje de instalación
       deferredPrompt.prompt();
+      // Esperar a que el usuario responda a la solicitud de instalación
       deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
           console.log('El usuario ha aceptado la instalación');
-          setIsInstalled(true);
+          setIsInstalled(true); // Actualizar el estado para ocultar el botón de instalación
         } else {
           console.log('El usuario ha cancelado la instalación');
         }
+        // Limpiar el objeto deferredPrompt
         setDeferredPrompt(null);
       });
     }
   };
 
   return (
-    <div id="container-menu" className="container">
-      <OfflineMessage showOfflineMessage={!isOnline} />
-      
-      {isOnline && (
-        <>
-          <h4>Haga clic en el botón para ver las instrucciones de uso</h4>
-          <div className="menuQr">
-            {qrImages.map((item, index) => (
-              <Tarjeta key={index} nombre={item.nombre} qrImage={item.imagen} grupo={item.grupo} medicamentos={item.medicamentos} />
-            ))}
-          </div>
-          
-          {!isInstalled && (
-            <div id="sombra">
-              <h4 id="verde">Haga clic en el botón para descargar la página como una App</h4>
-              <button className="button" id="botonInstalarPWA" onClick={mostrarMensajeInstalacion}>
-                Instalar App
-              </button>
-            </div>
-          )}
-        </>
-      )}
+    <div id="container-menu" className="container"> {/* Contenedor principal */}
+
+      <h4>Haga clic en el botón para ver las instrucciones de uso</h4>
+
+      <div className="menuQr"> {/* Contenedor específico del menú QR */}
+        {/* Mapear las imágenes QR */}
+        {qrImages.map((item, index) => (
+          <Tarjeta key={index} nombre={item.nombre} qrImage={item.imagen} grupo={item.grupo} medicamentos={item.medicamentos} />
+        ))}
+      </div>
+
+
+     {!isInstalled && <div id="sombra">
+      <h4 id="verde">Haga clic en el botón para descargar la página como una App</h4>
+      <button className="button" id="botonInstalarPWA" onClick={mostrarMensajeInstalacion}>Instalar App </button>
+
+     </div>}
+
     </div>
   );
 }
 
 export default MenuQR;
+
