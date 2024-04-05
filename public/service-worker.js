556be41440efe7a013d6e09b459df7468c28e511
MartuV <serranomv@hotmail.com>
2024-04-05 00:29:48 -0300

    mensaje error sin conexion prueba2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,126 +1,17 @@
 // Archivo service-worker.js
 const CACHE_NAME = 'mi-app-cache-v1';
+const offlinePage = '/offline.html';
 
-// Lista de archivos estáticos a cachear
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-];
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.add(offlinePage))
+  );
+});
 
-// Obtener dinámicamente los archivos CSS en la carpeta estilos
-const estilosFiles = [];
-const estilosPath = '/estilos'; // Ruta relativa a la carpeta estilos
-
-// Hacer una petición al servidor para obtener la lista de archivos CSS
-fetch(estilosPath)
-  .then(response => response.text())
-  .then(text => {
-    // Parsear el HTML de la carpeta estilos para extraer los archivos CSS
-    const parser = new DOMParser();
-    const htmlDocument = parser.parseFromString(text, 'text/html');
-    const links = htmlDocument.querySelectorAll('a[href$=".css"]'); // Seleccionar todos los enlaces a archivos CSS
-    links.forEach(link => {
-      estilosFiles.push(estilosPath + '/' + link.getAttribute('href')); // Agregar la ruta relativa del archivo CSS a la lista
-    });
-
-    // Agregar los archivos CSS a la lista urlsToCache
-    urlsToCache.push(...estilosFiles);
-  });
-
-// Obtener dinámicamente las imágenes en la carpeta imagenes
-const imagenesFiles = [];
-const imagenesPath = '/imagenes'; // Ruta relativa a la carpeta imagenes
-
-// Hacer una petición al servidor para obtener la lista de imágenes
-fetch(imagenesPath)
-  .then(response => response.text())
-  .then(text => {
-    // Parsear el HTML de la carpeta imagenes para extraer los enlaces a las imágenes
-    const parser = new DOMParser();
-    const htmlDocument = parser.parseFromString(text, 'text/html');
-    const links = htmlDocument.querySelectorAll('a[href$=".png"], a[href$=".jpg"], a[href$=".jpeg"], a[href$=".gif"]'); // Seleccionar todos los enlaces a imágenes
-    links.forEach(link => {
-      imagenesFiles.push(imagenesPath + '/' + link.getAttribute('href')); // Agregar la ruta relativa de la imagen a la lista
-    });
-
-    // Agregar las imágenes a la lista urlsToCache
-    urlsToCache.push(...imagenesFiles);
-  });
-
-
-
-  self.addEventListener('install', event => {
-    // Instalar el Service Worker
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(urlsToCache))
-    );
-  });
-  
-  // Variable para almacenar si el mensaje de instalación se ha mostrado
-  let mensajeInstalacionMostrado = false;
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      // Intenta obtener la respuesta de la caché
-      caches.match(event.request)
-        .then(response => {
-          // Si hay una respuesta en la caché, la retorna
-          if (response) {
-            return response;
-          }
-  
-          // Si no hay respuesta en la caché, intenta obtenerla de la red
-          return fetch(event.request)
-            .catch(() => {
-              // Si no hay conexión a Internet, muestra el mensaje de error
-              mostrarMensajeOffline();
-            });
-        })
-    );
-  });
-  
-  function mostrarMensajeOffline() {
-    // Muestra el mensaje de error en la página principal
-    const offlineMessage = document.getElementById('offline-message');
-    offlineMessage.style.display = 'block';
-  }
-  
-  
-  self.addEventListener('beforeinstallprompt', (event) => {
-    // Previene que el navegador muestre automáticamente el mensaje de instalación
-    event.preventDefault();
-    // Almacena el indicador en el almacenamiento local del navegador
-    if (!mensajeInstalacionMostrado) {
-      mensajeInstalacionMostrado = true;
-      mostrarMensajeInstalacion();
-    }
-  });
-  
-  function mostrarMensajeInstalacion() {
-    // Aquí puedes mostrar el mensaje de instalación, por ejemplo, en respuesta a una acción del usuario
-    // Por ejemplo, podrías mostrar un botón que diga "Instalar PWA" y al hacer clic en él, llamas a prompt() en el evento deferredPrompt
-    const botonInstalarPWA = document.getElementById('botonInstalarPWA');
-    // Verificar si la aplicación ya está instalada
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-      // Si está instalada, ocultar el botón de instalación
-      botonInstalarPWA.style.display = 'none';
-    } else {
-      // Si no está instalada, mostrar el botón de instalación
-      botonInstalarPWA.addEventListener('click', () => {
-        // Muestra el mensaje de instalación
-        deferredPrompt.prompt();
-        // Espera a que el usuario responda a la solicitud de instalación
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('El usuario ha aceptado la instalación');
-          } else {
-            console.log('El usuario ha cancelado la instalación');
-          }
-          // Limpia el objeto deferredPrompt
-          deferredPrompt = null;
-        });
-      });
-    }
-  }
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(offlinePage))
+  );
+});
--- src/componentes/MenuQR.js
+++ src/componentes/MenuQR.js
@@ -4,7 +4,6 @@ import '../estilos/menuQR.css';
 import Tarjeta from './Tarjeta';
 
 function MenuQR() {
-  // Datos de ejemplo de imágenes QR
   const qrImages = [
     { nombre: 'Instrucciones 1', imagen: 'jarabe.png', grupo: 'Antibióticos Jarabe - Polvo para Reconstituir Vía Oral', medicamentos: 'Amoxicilina - Amoxicilina/Clavulánico - Cefalexina - Claritromicina' },
     { nombre: 'Instrucciones 2', imagen: 'inhalador.png', grupo: 'Aerosoles y Aerocámaras', medicamentos: 'Budesonide - Salbutamol' },
@@ -15,18 +14,14 @@ function MenuQR() {
 
   useEffect(() => {
     const beforeInstallPromptHandler = (event) => {
-      // Prevenir que el navegador muestre automáticamente el mensaje de instalación
       event.preventDefault();
-      // Guardar el evento para usarlo más tarde
       setDeferredPrompt(event);
     };
 
     const appInstalledHandler = () => {
-      // La aplicación se ha instalado, actualiza el estado para ocultar el botón de instalación
       setIsInstalled(true);
     };
 
-    // Verificar si la PWA ya está instalada en el dispositivo
     if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true) {
       setIsInstalled(true);
     }
@@ -42,47 +37,42 @@ function MenuQR() {
 
   const mostrarMensajeInstalacion = () => {
     if (deferredPrompt) {
-      // Mostrar el mensaje de instalación
       deferredPrompt.prompt();
-      // Esperar a que el usuario responda a la solicitud de instalación
       deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
           console.log('El usuario ha aceptado la instalación');
-          setIsInstalled(true); // Actualizar el estado para ocultar el botón de instalación
+          setIsInstalled(true);
         } else {
           console.log('El usuario ha cancelado la instalación');
         }
-        // Limpiar el objeto deferredPrompt
         setDeferredPrompt(null);
       });
     }
   };
 
   return (
-    <div id="container-menu" className="container"> {/* Contenedor principal */}
-
+    <div id="container-menu" className="container">
       <h4>Haga clic en el botón para ver las instrucciones de uso</h4>
-
-      <div className="menuQr"> {/* Contenedor específico del menú QR */}
-        {/* Mapear las imágenes QR */}
+      <div className="menuQr">
         {qrImages.map((item, index) => (
           <Tarjeta key={index} nombre={item.nombre} qrImage={item.imagen} grupo={item.grupo} medicamentos={item.medicamentos} />
         ))}
       </div>
-
-
-     {!isInstalled && <div id="sombra">
-      <h4 id="verde">Haga clic en el botón para descargar la página como una App</h4>
-      <button className="button" id="botonInstalarPWA" onClick={mostrarMensajeInstalacion}>Instalar App </button>
-
-     </div>}
-
-     
-
-
+      {!navigator.onLine && (
+        <div id="sombra">
+          <h4 id="verde">No hay conexión a Internet</h4>
+        </div>
+      )}
+      {!isInstalled && (
+        <div id="sombra">
+          <h4 id="verde">Haga clic en el botón para descargar la página como una App</h4>
+          <button className="button" id="botonInstalarPWA" onClick={mostrarMensajeInstalacion}>Instalar App</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default MenuQR;
+
 
