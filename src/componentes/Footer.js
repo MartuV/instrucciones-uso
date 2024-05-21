fcbb4ce1c61a539c10118dda81ffc79de887e449
MartuV <serranomv@hotmail.com>
2024-05-21 13:02:01 -0300

    agregue autor logo footer

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -11,25 +11,31 @@ function Footer() {
                 <p>Desarrollo Web: Farm. Marta Victoria Serrano.</p> 
                 <p>Colaboración: Farm. María Fernanda Ceballo.</p> 
                 <p>Agradecimientos: Farm. Karina Gonella - Farm. Alicia Capelli.</p>
+                <p>Logo División Farmacia: Daniela Felt.</p>
                 
-                <p>Bibliografía: Modo de uso de aerocámaras con máscara facial - Sociedad Argentina de Pediatría; 
-                <br/>
-                Preparación de medicamentos por vía oral Importancia del agua utilizada - Dra. Viviana Bologna, Farm. María Beatriz Cardoso;
-                <br/>
-                Prospecto AeroChamber Plus Flow-Vu. </p>
+                <p>Bibliografía:</p>
+                <p>
+                    Modo de uso de aerocámaras con máscara facial - Sociedad Argentina de Pediatría;<br/>
+                    Preparación de medicamentos por vía oral Importancia del agua utilizada - Dra. Viviana Bologna, Farm. María Beatriz Cardoso;<br/>
+                    Prospecto AeroChamber Plus Flow-Vu.
+                </p>
                 <div> 
-                Iconos: <a href="https://www.flaticon.es/autores/eucalyp" title="Eucalyp"> Eucalyp</a> - <a href="https://www.flaticon.es/autores/freepik" title="Freepik"> Freepik</a> - <a href="https://www.flaticon.es/iconos-gratis/asma" title="asma iconos"> Smashicons</a> - <a href="https://www.flaticon.es/iconos-gratis/repetir" title="repetir iconos"> heisenberg_jr</a> - <a href="https://www.flaticon.es/iconos-gratis/sentado" title="sentado iconos">
-                Muhammad_Usman</a> - <a href="https://www.flaticon.es/autores/payungkead" title="Payungkead"> Payungkead</a> - <a href="https://www.flaticon.es/iconos-gratis/inhalador" title="inhalador iconos"> bsd </a> 
-                from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es'.</a>
+                    Iconos: 
+                    <a href="https://www.flaticon.es/autores/eucalyp" title="Eucalyp"> Eucalyp</a> - 
+                    <a href="https://www.flaticon.es/autores/freepik" title="Freepik"> Freepik</a> - 
+                    <a href="https://www.flaticon.es/iconos-gratis/asma" title="asma iconos"> Smashicons</a> - 
+                    <a href="https://www.flaticon.es/iconos-gratis/repetir" title="repetir iconos"> heisenberg_jr</a> - 
+                    <a href="https://www.flaticon.es/iconos-gratis/sentado" title="sentado iconos"> Muhammad_Usman</a> - 
+                    <a href="https://www.flaticon.es/autores/payungkead" title="Payungkead"> Payungkead</a> - 
+                    <a href="https://www.flaticon.es/iconos-gratis/inhalador" title="inhalador iconos"> bsd </a>
+                    from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a>.
                 </div>
- 
             </div>
             <div id="footer-year">
-                <p>&copy; 2024 División Farmacia Hospital de Niños Dr. Ricardo Gutierréz</p>
-                <p></p>
+                <p>&copy; 2024 División Farmacia Hospital de Niños Dr. Ricardo Gutiérrez</p>
             </div>
-           
         </footer>
+
     );
 }
 
